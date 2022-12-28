42b0557a1588b22bcd0ccfec75c13f5bbf9aa0fb
progIfe <ifeoluwasamson90@gmail.com>
2022-12-28 19:06:21 +0100

    Meal model Configuration and admin customization

--- meals/admin.py
+++ meals/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import category, Meal
+from django.utils.html import format_html
 
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        try:
+            return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.image.url))
+        except:
+            pass
+    thumbnail.short_description = 'photo'
+
+    list_display = ('id', 'thumbnail', 'name')
+    list_display_links = ('name', 'thumbnail')
+    search_fields = ('name')
+    
+class MealAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+    def thumbnail(self, object):
+        try:
+            return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.image.url))
+        except:
+            pass
+    thumbnail.short_description = 'photo'
+
+    list_display = ('id', 'thumbnail', 'name',
+                    'price', 'preparation_time')
+    list_display_links = ('name', 'thumbnail')
+    search_fields = ('name')
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Meal, MealAdmin)
--- meals/models.py
+++ meals/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='media/categories/%y/%m/%d/', blank=True)
+    
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        
+    def __str__(self):
+        return self.name
+    
+
+class Meal(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=500)
+    image = models.ImageField(upload_to='media/meals/%y/%m/%d/', blank=True)
+    persons = models.IntegerField()
+    price = models.DecimalField(max_digits=5)
+    preparation_time = models.IntegerField()
+    slug = models.SlugField(max_length=100, unique=True)
+
+
+    def __str__(self):
+        return self.name
