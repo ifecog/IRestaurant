585edfda6c3f33e005fefbded9107ef7f6573d4e
progIfe <ifeoluwasamson90@gmail.com>
2022-12-28 21:06:43 +0100

    Dynamic home page configuration

--- meals/views.py
+++ meals/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from . import models
+from . models import Feature, Testimonial
 
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    features = Feature.objects.all()
+    testimonials = Testimonial.objects.all()
+    context = {
+        'features': features,
+        'testimonials': testimonials,
+    }
+    
+    return render(request, 'pages/home.html', context)
