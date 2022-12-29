c40c0c6663e7c5f0b72caad53859e24179d5d550
progIfe <ifeoluwasamson90@gmail.com>
2022-12-29 21:55:30 +0100

    Configuration of smtp email

--- contact/views.py
+++ contact/views.py
@@ -1,33 +1,45 @@
-from django.shortcuts import render, redirect
-from .models import Contact
-from django.core.mail import send_mail
 from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import ContactForm
 from irestaurant import settings
 
+
 # Create your views here.
+
+
 def contact(request):
     if request.method == 'POST':
-        name = request.POST['name']
-        email = request.POST['email']
-        subject = request.POST['subject']
-        message = request.POST['message']
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['name']
 
-        contact = Contact(name=name, email=email, subject=subject, message=message)
+            try:
+                subject = [subject]
+                message = [message]
+                from_email = email
+                recipient_list = [settings.EMAIL_HOST_USER]
+                send_mail(subject, message, from_email,
+                          recipient_list, fail_silently=False)
 
-        subject = [subject]
-        message = [message]
-        from_email = email
-        recipient_list = [settings.EMAIL_HOST_USER]
-        send_mail(subject, message, from_email,
-                  recipient_list, fail_silently=False)
+                form.save()
 
-        contact.save()
+                messages.success(
+                    request, 'Your inquiry has been submitted. We will get back to you shortly')
+                return redirect('contact')
 
-        messages.success(
-            request, 'Your inquiry has been submitted. We will get back to you shortly')
-        return redirect('contact')
+            except BadHeaderError:
+                return HttpResponse('Bad Response')
 
-    return render(request, 'pages/contact.html')
+    else:
+        form = ContactForm()
 
+    context = {
+        'form': form,
+    }
 
-
+    return render(request, 'pages/contact.html', context)
