e256131911932c8a84eafe9fd3d277689bb2270c
progIfe <ifeoluwasamson90@gmail.com>
2022-12-29 16:23:26 +0100

    about app views.py modification

--- about/views.py
+++ about/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from .models import Chef
+from .models import Chef, About
 
 # Create your views here.
 
 
 def about(request):
+    abouts = About.objects.all()
     chefs = Chef.objects.all()
-
     context = {
+        'abouts': abouts,
         'chefs': chefs,
     }
 
-    return render(request, 'pages/about.html')
+    return render(request, 'pages/about.html', context)
