5a9e4f112fdbe5217855bc531ced4858f7f881d7
progIfe <ifeoluwasamson90@gmail.com>
2022-12-29 16:09:15 +0100

    about app creation and configuration

--- about/models.py
+++ about/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from ckeditor.fields import RichTextField
 
 # Create your models here.
+
+
 class Chef(models.Model):
     name = models.CharField(max_length=50)
     role = models.CharField(max_length=50)
@@ -9,13 +11,14 @@ class Chef(models.Model):
     facebook_link = models.URLField(max_length=100)
     twitter_link = models.URLField(max_length=100)
     google_link = models.URLField(max_length=100)
-    
+
     def __str__(self):
         return self.name
-    
+
+
 class About(models.Model):
     name = models.CharField(max_length=50)
     body = RichTextField()
-    
+
     def __str__(self):
         return self.name
